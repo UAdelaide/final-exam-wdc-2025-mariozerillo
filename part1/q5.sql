6b18288abe7e42073023ca572dcb5143ebb8cb41
mariozerillo <105763914+mariozerillo@users.noreply.github.com>
2025-06-20 06:50:57 +0000

    [automatic commit] 2025-06-20 06:50:57 UTC

--- part1/q5.sql
+++ part1/q5.sql
@@ -3,4 +3,5 @@ VALUES
     ('alice123', 'alice@example.com', 'hashed123', 'owner'),
     ('bobwalker', 'bob@example.com', 'hashed456', 'walker'),
     ('carol123', 'carol@example.com', 'hashed789', 'owner'),
-    ('johnsmith')
+    ('johnsmith', 'john@example.com', 'hashedABC', 'walker'),
+    ('jane123)
