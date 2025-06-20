4c7bd50d91293065ab8a5b5a63cfec310480ddd2
mariozerillo <105763914+mariozerillo@users.noreply.github.com>
2025-06-20 07:10:34 +0000

    [automatic commit] 2025-06-20 07:10:34 UTC

--- part1/q5.sql
+++ part1/q5.sql
@@ -16,8 +16,8 @@ VALUES
 
 INSERT INTO WalkRequests (dog_id, requested_time, duration_minutes, location, status)
 VALUES
-    SELECT dog_id, '2025-06-10 08:00:00', 30, 'Parklands', 'open' FROM Dogs WHERE name = 'Max'),
-    (SELECT dog_id, '2025-06-10 09:30:00', 45, 'Beachside Ave', 'accepted' FROM Dogs WHERE name = 'Bella'),
-    (SELECT dog_id, '2025-06-10 10:00:00', 15, 'Seaton', 'rejected' FROM Dogs WHERE name = 'Scooby'),
+    SELECT dog_id, '2025-06-10 08:00:00', 30, 'Parklands', 'open' FROM Dogs WHERE name = 'Max' UNION ALL
+    (SELECT dog_id, '2025-06-10 09:30:00', 45, 'Beachside Ave', 'accepted' FROM Dogs WHERE name = 'Bella' UNION ALL
+    (SELECT dog_id, '2025-06-10 10:00:00', 15, 'Seaton', 'rejected' FROM Dogs WHERE name = 'Scooby' UNION ALL
     (SELECT dog_id, '2025-06-10 10:30:00', 20, 'Fulham', 'open' FROM Dogs WHERE name = 'Scrappy'),
     (SELECT dog_id, '2025-06-10 11:30:00', 60, 'West Lakes Shore', 'open' FROM Dogs WHERE name = 'Buster');
