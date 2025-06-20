e9a9b91ed66262a569d42280bcab08fb116fdbf9
mariozerillo <105763914+mariozerillo@users.noreply.github.com>
2025-06-20 07:26:54 +0000

    [automatic commit] 2025-06-20 07:26:54 UTC

--- part1/app.js
+++ part1/app.js
@@ -40,7 +40,6 @@ let db;
       password: '',
       database: 'testdb'
     });
-
   } catch (err) {
     console.error('Error setting up database. Ensure Mysql is running: service mysql start', err);
   }
