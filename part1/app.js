d852aff6b5cdd5f0e02380d5f66196b507907987
mariozerillo <105763914+mariozerillo@users.noreply.github.com>
2025-06-20 07:20:26 +0000

    [automatic commit] 2025-06-20 07:20:26 UTC

--- part1/app.js
+++ part1/app.js
@@ -14,6 +14,29 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+let db;
+
+(async () => {
+  try {
+    // Connect to MySQL without specifying a database
+    const connection = await mysql.createConnection({
+      host: 'localhost',
+      user: 'root',
+      password: '' // Set your MySQL root password
+    });
+
+    // Create the database if it doesn't exist
+    await connection.query('CREATE DATABASE IF NOT EXISTS testdb');
+    await connection.end();
+
+    // Now connect to the created database
+    db = await mysql.createConnection({
+      host: 'localhost',
+      user: 'root',
+      password: '',
+      database: 'testdb'
+    });
+
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
