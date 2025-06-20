9369f6d87f3fa3b8297eb41e81bf49baf6a0b3af
mariozerillo <105763914+mariozerillo@users.noreply.github.com>
2025-06-20 06:56:08 +0000

    [automatic commit] 2025-06-20 06:56:08 UTC

--- part1/q5.sql
+++ part1/q5.sql
@@ -9,4 +9,4 @@ VALUES
 INSERT INTO Dogs (owner_id, name, size)
 VALUES
     (SELECT user_id, 'Max', 'medium' FROM Users WHERE username = 'alice123')
-    (SELECT user_id, '')
+    (SELECT user_id, 'Bella', 'small' FROM Users WHERE username = 'carol123')
