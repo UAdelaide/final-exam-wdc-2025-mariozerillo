2e64feda8ae2fa4a59f443d38fd8c640d26077da
mariozerillo <105763914+mariozerillo@users.noreply.github.com>
2025-06-20 07:32:42 +0000

    [automatic commit] 2025-06-20 07:32:41 UTC

--- part1/app.js
+++ part1/app.js
@@ -34,7 +34,7 @@ const createDB = fs.readFileSync('dogwalks.sql', 'utf-8');
     });
 
     // Create the database if it doesn't exist
-    await connection.execute(dogwalks.sql);
+    await connection.query(createDB);
     await connection.end();
 
     // Now connect to the created database
