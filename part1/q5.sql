1ccb89e12c5bb4775521ed134e4bc9d19d6f6eac
mariozerillo <105763914+mariozerillo@users.noreply.github.com>
2025-06-20 06:58:30 +0000

    [automatic commit] 2025-06-20 06:58:30 UTC

--- part1/q5.sql
+++ part1/q5.sql
@@ -8,8 +8,10 @@ VALUES
 
 INSERT INTO Dogs (owner_id, name, size)
 VALUES
-    (SELECT user_id, 'Max', 'medium' FROM Users WHERE username = 'alice123')
-    (SELECT user_id, 'Bella', 'small' FROM Users WHERE username = 'carol123')
-    (SELECT user_id, 'Scooby', 'large' FROM Users WHERE username = 'jane123')
-    (SELECT user_id, 'Scrappy', 'medium' FROM Users WHERE username = 'carol123')
+    (SELECT user_id, 'Max', 'medium' FROM Users WHERE username = 'alice123'),
+    (SELECT user_id, 'Bella', 'small' FROM Users WHERE username = 'carol123'),
+    (SELECT user_id, 'Scooby', 'large' FROM Users WHERE username = 'jane123'),
+    (SELECT user_id, 'Scrappy', 'medium' FROM Users WHERE username = 'carol123'),
     (SELECT user_id, 'Buster', 'medium' FROM Users WHERE username = 'jane123');
+
+INSERT INTO WalkRequests ()
