0318bff0b6932f7b73d2a4256850f363c69a5101
mariozerillo <105763914+mariozerillo@users.noreply.github.com>
2025-06-20 07:01:18 +0000

    [automatic commit] 2025-06-20 07:01:18 UTC

--- part1/q5.sql
+++ part1/q5.sql
@@ -16,4 +16,5 @@ VALUES
 
 INSERT INTO WalkRequests (dog_id, requested_time, duration_minutes, location, status)
 VALUES
-    (SELECT dog_id, '2025-06-10 08:00:00', 30, 'Parklands', 'open' FROM Dogs WHERE name = )
+    (SELECT dog_id, '2025-06-10 08:00:00', 30, 'Parklands', 'open' FROM Dogs WHERE name = 'Max'),
+    (SELECT dog_id, '2025-06-10 09:30:00')
