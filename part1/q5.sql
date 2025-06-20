419329dd9d03acceddfa289fdbbf091abd8a2f43
mariozerillo <105763914+mariozerillo@users.noreply.github.com>
2025-06-20 07:11:35 +0000

    [automatic commit] 2025-06-20 07:11:34 UTC

--- part1/q5.sql
+++ part1/q5.sql
@@ -8,7 +8,7 @@ VALUES
 
 INSERT INTO Dogs (owner_id, name, size)
 VALUES
-    (SELECT user_id, 'Max', 'medium' FROM Users WHERE username = 'alice123' UNION ALL
+    SELECT user_id, 'Max', 'medium' FROM Users WHERE username = 'alice123' UNION ALL
     SELECT user_id, 'Bella', 'small' FROM Users WHERE username = 'carol123' UNION ALL
     SELECT user_id, 'Scooby', 'large' FROM Users WHERE username = 'jane123' UNION ALL
     SELECT user_id, 'Scrappy', 'medium' FROM Users WHERE username = 'carol123' UNION ALL
