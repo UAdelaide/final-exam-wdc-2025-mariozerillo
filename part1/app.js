7f206b8b542f9df9170607a456353e349131af9e
mariozerillo <105763914+mariozerillo@users.noreply.github.com>
2025-06-20 07:20:46 +0000

    [automatic commit] 2025-06-20 07:20:46 UTC

--- part1/app.js
+++ part1/app.js
@@ -2,17 +2,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-
-var app = express();
-
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+var mysql = require('mysql2/promise');
 
 let db;
 
@@ -36,6 +26,21 @@ let db;
       password: '',
       database: 'testdb'
     });
+
+
+
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+
+var app = express();
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
