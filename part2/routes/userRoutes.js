6078c2cfd4306987c16bda6d135156e52a7e187e
mariozerillo <105763914+mariozerillo@users.noreply.github.com>
2025-06-21 06:19:17 +0000

    [automatic commit] 2025-06-21 06:19:16 UTC

--- part2/routes/userRoutes.js
+++ part2/routes/userRoutes.js
@@ -85,8 +85,24 @@ router.get('/fetchDogs', async(req, res) => {
 
 });
 
-router.get('/dogs', async(req, res) => {
+router.get('/dogs', async(req, res, next) => {
+    try {
+        let db = await mysql.createConnection({
+            host: 'localhost',
+            user: 'root',
+            password: '',
+            database: 'DogWalkService'
+        });
 
+        const fetchDogsSql = 'SELECT d.name AS dog_name, d.size, (SELECT u.username FROM Users AS u WHERE d.owner_id = u.user_id) AS owner_username FROM Dogs AS d';
+
+        const [result] = await db.execute(fetchDogsSql);
+
+        res.json(result);
+
+    } catch (error) {
+        return res.status(500).json({ error: 'Internal server error.' });
+    }
 });
 
 module.exports = router;
