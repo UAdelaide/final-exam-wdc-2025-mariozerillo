990c3eb96482cbd0062bc450a304a979e9acce1f
mariozerillo <105763914+mariozerillo@users.noreply.github.com>
2025-06-21 03:03:50 +0000

    [automatic commit] 2025-06-21 03:03:50 UTC

--- part2/app.js
+++ part2/app.js
@@ -8,7 +8,7 @@ const app = express();
 // Middleware
 app.use(express.json());
 app.use(express.static(path.join(__dirname, '/public')));
-app.use(session(){
+app.use(session({
     secret: 'abcdef',
     resave: false,
     saveUninitialized: false
