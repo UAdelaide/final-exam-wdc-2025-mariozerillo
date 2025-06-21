7e9807afd31176d0ff8ee24b47b92cca595d32df
mariozerillo <105763914+mariozerillo@users.noreply.github.com>
2025-06-21 01:29:08 +0000

    [automatic commit] 2025-06-21 01:29:08 UTC

--- part1/routes/api.js
+++ part1/routes/api.js
@@ -15,7 +15,7 @@ router.get('/dogs', async(req, res, next) => {
             database: 'DogWalkService'
         });
 
-
+        const fetchDogsSql = ''
 
     } catch (error) {
         return res.sendStatus(500).json('Internal server error.');
