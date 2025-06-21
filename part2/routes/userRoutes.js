4ad3f85d03bdbcac74fcdfa9d9e723c6f1691da4
mariozerillo <105763914+mariozerillo@users.noreply.github.com>
2025-06-21 06:23:27 +0000

    [automatic commit] 2025-06-21 06:23:27 UTC

--- part2/routes/userRoutes.js
+++ part2/routes/userRoutes.js
@@ -107,9 +107,9 @@ router.get('/fetchImages', async(req, res) => {
     const data = await response.json();
 
     res.json(data);
-  } catch (error) (
-      res.status(500).json({ error: 'Internal server error.' });
-  )
+  } catch (error) {
+    return res.status(500).json({ error: 'Internal server error.' });
+  }
 });
 
 module.exports = router;
