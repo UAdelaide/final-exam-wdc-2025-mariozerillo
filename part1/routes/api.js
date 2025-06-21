2cc0fdde70ba95abc23b37a010e43502ea911eea
mariozerillo <105763914+mariozerillo@users.noreply.github.com>
2025-06-21 01:24:01 +0000

    [automatic commit] 2025-06-21 01:24:01 UTC

--- part1/routes/api.js
+++ part1/routes/api.js
@@ -5,6 +5,8 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
-router.get('/')
+router.get('/dogs', function(req, res, next) {
+    
+});
 
 module.exports = router;
