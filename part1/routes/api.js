fd1f1041df3bffb2f90eddf7bd0b66f0b43801dc
mariozerillo <105763914+mariozerillo@users.noreply.github.com>
2025-06-21 01:42:27 +0000

    [automatic commit] 2025-06-21 01:42:27 UTC

--- part1/routes/api.js
+++ part1/routes/api.js
@@ -17,9 +17,9 @@ router.get('/dogs', async(req, res, next) => {
 
         const fetchDogsSql = 'SELECT d.name, d.size, (SELECT u.username FROM Users AS u WHERE d.owner_id = u.user_id) AS owner_username FROM Dogs AS d';
 
-        const [result] = await db.Promise().execute(fetchDogsSql);
+        const [result] = await db.execute(fetchDogsSql);
 
-        return res.json(result);
+        res.json(result);
 
     } catch (error) {
         return res.sendStatus(500).json('Internal server error.');
