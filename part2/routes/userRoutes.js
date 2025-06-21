eb3c3f9616425b6fc0d61c6e4e3b4c7a1d41d2bc
mariozerillo <105763914+mariozerillo@users.noreply.github.com>
2025-06-21 04:46:29 +0000

    [automatic commit] 2025-06-21 04:46:29 UTC

--- part2/routes/userRoutes.js
+++ part2/routes/userRoutes.js
@@ -33,8 +33,8 @@ router.get('/me', (req, res) => {
   if (!req.session.user) {
     return res.status(401).json({ error: 'Not logged in' });
   }
-  
-  res.json(req.session.user);
+  const id = req.session.user.id;
+  res.json(id);
 });
 
 // POST login (dummy version)
